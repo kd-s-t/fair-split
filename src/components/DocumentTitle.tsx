d680f30ad94d8d74bb75b82fa4e9bbaaaaab9e4f
Ken Dan Tinio <kendantinio@gmail.com>
2025-08-06 10:47:38 +0800

    ecr image fix

--- src/components/DocumentTitle.tsx
+++ src/components/DocumentTitle.tsx
@@ -4,8 +4,12 @@ import { useEffect } from 'react'
 
 export default function DocumentTitle() {
   useEffect(() => {
-    // Set the document title for staging environment
-    document.title = 'Safe Split Local'
+    // Set the document title based on environment
+    if (process.env.NODE_ENV === 'staging') {
+      document.title = 'Safe Split Local'
+    } else {
+      document.title = 'SafeSplit'
+    }
   }, [])
 
   return null
